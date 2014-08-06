143a137eda74437d46331a42c14c8885902a1913
sunday35034 <lwc_evale@hotmail.com>
2014-08-06 09:29:57 +0800

    完成用户列表里投资总额和收益总额的统计功能；首页的箭头增加颜色更直观

--- app/models/income.rb
+++ app/models/income.rb
@@ -2,6 +2,8 @@ class Income < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
 
+  scope :total_amount, Proc.new{ |uid| where(user_id: uid).inject(0) {|sum, el| sum += el.amount} }
+  
   after_save do
     Invest.add_to_account(amount: amount, product_id: product_id, user_id: user_id)
   end
